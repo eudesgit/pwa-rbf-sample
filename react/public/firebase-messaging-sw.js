3e3912638f07e78cb7618e1b3d8bd398af013566
Eudes <eudes@win7>
2018-04-13 10:39:28 -0400

    Package and manifest changes

--- react/public/firebase-messaging-sw.js
+++ react/public/firebase-messaging-sw.js
@@ -1,7 +1,9 @@
 importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
 
-import config from "./firebase-messaging-sw-config";
+config = {
+    messagingSenderId: "885118303528"
+}
 firebase.initializeApp(config)
 
 const messaging = firebase.messaging()
