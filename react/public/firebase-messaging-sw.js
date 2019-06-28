22fcccc6c3269214835d84794b124896b8ddf62c
Eudes <eudes@win7>
2019-06-28 14:38:01 -0400

    Updating React and Firebase versions

--- react/public/firebase-messaging-sw.js
+++ react/public/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/6.2.3/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/6.2.3/firebase-messaging.js');
 
 // Initialize Firebase
 var config = {
